7d2cf85c4ea266f7f639c57eefe60d92b997be93
ntbloom <ntbloom@gmail.com>
2020-10-25 09:23:06 -0400

    fixed select statement

--- sql/sample_query.sql
+++ sql/sample_query.sql
@@ -1,17 +1,13 @@
 /* sample query for entering a tick */
 
 BEGIN;
-    /*
-    INSERT INTO ticks (num) VALUES (1);
-    INSERT INTO ticks (num) VALUES (1);
-    INSERT INTO ticks (num) VALUES (1);
-    INSERT INTO ticks (num) VALUES (1);
-    */
-    INSERT INTO ticks (num) VALUES (0.2794);
-    INSERT INTO ticks (num) VALUES (0.2794);
-    INSERT INTO ticks (num) VALUES (0.2794);
-    INSERT INTO ticks (num) VALUES (0.2794);
-
+    INSERT INTO ticks (gauge, num) SELECT 1, mm_per_click FROM gauge WHERE id = 1;
+    INSERT INTO ticks (gauge, num) SELECT 1, mm_per_click FROM gauge WHERE id = 1;
+    INSERT INTO ticks (gauge, num) SELECT 1, mm_per_click FROM gauge WHERE id = 1;
+    INSERT INTO ticks (gauge, num) SELECT 1, mm_per_click FROM gauge WHERE id = 1;
+    INSERT INTO ticks (gauge, num) SELECT 1, mm_per_click FROM gauge WHERE id = 1;
+    INSERT INTO ticks (gauge, num) SELECT 1, mm_per_click FROM gauge WHERE id = 1;
 COMMIT;
 
-SELECT SUM(num) FROM TICKS;
+SELECT SUM(num) FROM ticks AS total_rain;
+SELECT COUNT(num) FROM ticks as clicks;
