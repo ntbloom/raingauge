3b65dd538c77d29e558776363e47d86dbd2f039f
ntbloom <ntbloom@gmail.com>
2020-10-25 17:37:53 -0400

    proper error handling on connection

--- src/dbconnect.c
+++ src/dbconnect.c
@@ -3,7 +3,7 @@
 PGconn* dbconnect(const char* conninfo) {
     PGconn* conn = PQconnectdb(conninfo);
     if (PQstatus(conn) != CONNECTION_OK) {
-        fprintf(stderr, "failure to connect\n");
+        fprintf(stderr, "failure to connect: %s\n", PQerrorMessage(conn));
         return NULL;
     }
     return conn;
