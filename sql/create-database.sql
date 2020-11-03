18e343c668a99587a2f3a96384f36fff5c13b299
ntbloom <ntbloom@gmail.com>
2020-11-02 22:36:09 -0500

    working on sqlite transition

--- sql/create-database.sql
+++
@@ -1,4 +0,0 @@
-/* database.sql -- create new database */
-
-DROP DATABASE IF EXISTS local;
-CREATE DATABASE local;
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,21 +1,18 @@
-/* schema.sql -- schema for the raingauge database */
-SET CLIENT_MIN_MESSAGES to WARNING;
-BEGIN;
-    DROP TABLE IF EXISTS gauge CASCADE;
-    CREATE TABLE gauge(
-        id INT PRIMARY KEY,
-        shortname TEXT NOT NULL,
-        mm_per_click NUMERIC DEFAULT 0.2794,
-        description TEXT NOT NULL,
-        created_at TIMESTAMPTZ DEFAULT current_timestamp,
-        UNIQUE(shortname)
-    );
+/* schema.sql -- schema for local sqlite file */
+DROP TABLE IF EXISTS gauge CASCADE;
+CREATE TABLE gauge(
+    id INT PRIMARY KEY,
+    shortname TEXT NOT NULL,
+    mm_per_click REAL DEFAULT 0.2794,
+    description TEXT NOT NULL,
+    created_at TEXT, /* timestamp */
+    UNIQUE(shortname)
+);
 
-    DROP TABLE IF EXISTS ticks CASCADE;
-    CREATE TABLE ticks (
-        db_time TIMESTAMPTZ DEFAULT current_timestamp,
-        gauge INT DEFAULT 1,
-        num NUMERIC NOT NULL,
-        FOREIGN KEY (gauge) REFERENCES gauge(id)
-    );
-COMMIT;
+DROP TABLE IF EXISTS ticks CASCADE;
+CREATE TABLE ticks (
+    db_time TEXT, /* current_timestamp */
+    gauge INT DEFAULT 1,
+    num REAL NOT NULL,
+    FOREIGN KEY (gauge) REFERENCES gauge(id)
+);
